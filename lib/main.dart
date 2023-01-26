8822c1959055c133d5be4f1b1acb668a18bef9ff
IKENNA COLLINS <obettaikenna19@gmail.com>
2023-01-26 18:45:55 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,26 +32,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  List course = [
+    "Intro",
+    "Beginners",
+    "Mediate",
+    "Advance",
+  ];
   List info = [];
+  List begin = [];
+  List intro = [];
+  List medium = [];
   bool _playingArea = false;
   late VideoPlayerController _controller;
-  _initData() async{
-   await DefaultAssetBundle.of(context).loadString("json/info.json").then((value) {
-     setState(() {
-       info = json.decode(value);
-     });
+
+  _initData() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/info.json")
+        .then((value) {
+      setState(() {
+        info = json.decode(value);
+      });
+    });
+  } //do these for all the courses json
+
+  _initData2() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/info.json")
+        .then((value) {
+      setState(() {
+        begin = json.decode(value);
+      });
     });
   }
 
+  _initData3() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/info.json")
+        .then((value) {
+      setState(() {
+        intro = json.decode(value);
+      });
+    });
+  }
+
+  _initData4() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/info.json")
+        .then((value) {
+      setState(() {
+        medium = json.decode(value);
+      });
+    });
+  } //do these for all the courses json
+
+  List<dynamic> videos = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _initData();
+    _initData2();
+    _initData3();
+    _initData4();
   }
 
+  int current = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -66,132 +112,226 @@ class _HomeState extends State<Home> {
             gradient: LinearGradient(
           colors: [Colors.blue, Colors.purple],
         )),
-        child: Column(children: [
-          _playingArea==false ?Container(
-            padding: EdgeInsets.only(top: 30,left: 30,right: 30),
-            width: MediaQuery.of(context).size.width,
-            height: 300,
-            child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-
-                Icon(Icons.play_circle_fill,color: Colors.white,),
-                Icon(Icons.info_outline,color: Colors.white,),
-              ],),
-              SizedBox(height: 30,),
-              Text("Please Select the Course Videos \n to continue",style: TextStyle(color: Colors.white,fontSize: 30),),
-
-            ],),
-          ) :
-          Container
-            (
-            child: Column(
-              children: [
-              Container(
-                height: 100,
-                padding: EdgeInsets.only(top: 50,left: 30,right: 30),
-                child: Row(
+        child: Column(
+          children: [
+            _playingArea == false
+                ? Container(
+                    padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.play_circle_fill,
+                              color: Colors.white,
+                            ),
+                            Icon(
+                              Icons.info_outline,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          "Please Select the Course Videos \n to continue",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          padding:
+                              EdgeInsets.only(top: 50, left: 30, right: 30),
+                          child: Row(
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  size: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              InkWell(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  size: 20,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        _playView(context),
+                      ],
+                    ),
+                  ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topRight: Radius.circular(70))),
+                child: Column(
                   children: [
-                    InkWell(onTap: (){},
-                    child: Icon(Icons.arrow_back_ios,size: 20,color: Colors.white,),),
-                    Expanded(child: Container()),
-                    InkWell(onTap: (){},
-                      child: Icon(Icons.arrow_back_ios,size: 20,color: Colors.white,),)
+                    SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: course.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      current = index;
+                                    });
+                                  },
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    height: 80,
+                                    width: 45,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      color: current == index
+                                          ? Colors.blue
+                                          : Colors.grey,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        course[index],
+                                        style: TextStyle(color: Colors.orange),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: ListView.builder(
+                                      itemCount: info.length,
+                                      itemBuilder: (_, index) {
+                                        return GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              _onTapVideo(index);
+                                              print(info[index]["url"]);
+                                              if (_playingArea == false) {
+                                                _playingArea = true;
+                                              }
+                                            });
+                                          },
+                                          child: Container(
+                                            height: 135,
+// color: Colors.redAccent,
+// width: 200,
+                                            child: Column(
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Container(
+                                                      width: 80,
+                                                      height: 80,
+                                                      decoration: BoxDecoration(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(10),
+                                                          image: DecorationImage(
+                                                              image: AssetImage(
+                                                                  info[index][
+                                                                      "Image"]))),
+                                                    ),
+                                                    SizedBox(
+                                                      width: 10,
+                                                    ),
+                                                    Column(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .center,
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(
+                                                          info[index]["title"],
+                                                          style: TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold,
+                                                              fontSize: 18),
+                                                        ),
+                                                        SizedBox(
+                                                          height: 10,
+                                                        ),
+                                                        Padding(
+                                                          padding:
+                                                              EdgeInsets.only(
+                                                                  top: 3),
+                                                          child: Text(
+                                                              info[index]
+                                                                  ["time"],
+                                                              style: TextStyle(
+                                                                  color: Colors
+                                                                          .grey[
+                                                                      500])),
+                                                        ),
+                                                      ],
+                                                    )
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      }),
+                                ),
+                              ],
+                            );
+                          }),
+                    )
                   ],
                 ),
               ),
-                _playView(context),
-            ],),
-
-          ),
-          Expanded(child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topRight: Radius.circular(70))
-            ),
-            child: Column(
-              children: [
-                Expanded
-                  (
-                    child: ListView.builder(
-                      itemCount:info.length,
-                    itemBuilder: (_, index){
-                  return GestureDetector(
-                    onTap: (){
-                     setState(() {
-                       _onTapVideo(index);
-                       print(info[index]["url"]);
-                      if(_playingArea==false){
-                        _playingArea = true;
-                      }
-                     });
-                    },
-                    child: Container(
-                      height: 135,
-                      // color: Colors.redAccent,
-                      // width: 200,
-                      child: Column(
-                        children: [
-                          Row(children: [
-                            Container(
-                              width: 80,
-                              height: 80,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(image: AssetImage(info[index]["Image"]))
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(info[index]["title"],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                                SizedBox(height: 10,),
-                                Padding(padding: EdgeInsets.only(top: 3),
-                                child: Text(info[index]["time"],style: TextStyle(color: Colors.grey[500])),),
-                              ],
-                            )
-                          ],)
-                        ],
-                      ),
-                    ),
-                  );
-                }))
-            ],),
-          ),
-          )
-        ],),
+            )
+          ],
+        ),
       ),
     );
   }
 
- Widget _playView(BuildContext context){
-final controller = _controller;
-if (controller!=null&&controller.value.isInitialized) {
-  return AspectRatio(
-    aspectRatio: 16/9,
-    // height:300,
-    // width: 300,
-    child: VideoPlayer(controller),);
-}  else{
-return  Text("No controller");
-}
+  Widget _playView(BuildContext context) {
+    final controller = _controller;
+    if (controller != null && controller.value.isInitialized) {
+      return AspectRatio(
+        aspectRatio: 16 / 9,
+        // height:300,
+        // width: 300,
+        child: VideoPlayer(controller),
+      );
+    } else {
+      return Text("No controller");
+    }
   }
 
-  _onTapVideo(int index){
-   final controller = VideoPlayerController.network(info[index]["url"]);
-   _controller = controller;
-   setState(() {
-
-   });
-   controller..initialize().then((_) {
-     controller.play();
-     setState(() {
-
-     });
-   });
+  _onTapVideo(int index) {
+    final controller = VideoPlayerController.network(info[index]["url"]);
+    _controller = controller;
+    setState(() {});
+    controller
+      ..initialize().then((_) {
+        controller.play();
+        setState(() {});
+      });
   }
 }
